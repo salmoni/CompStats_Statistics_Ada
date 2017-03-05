881d3fde6d0a6895697ed46f52a921f30a4ffb38
Alan J. Salmoni <salmoni@gmail.com>
2017-03-05 20:08:06 +0000

    Updated with new tests and a couple of bugfixes
    Introduced new tests:
    
    Get_Coefficient_of_variation
    Is_Value_In_Array
    Get_Unique_Values

--- CompStats_Statistics.adb
+++ CompStats_Statistics.adb
@@ -10,7 +10,7 @@ begin
    Count := Values'Length;
    return Count;
 end Get_Count;
-
+--------------------------------------------------------------
 
 function Get_Minimum (Values: Values_Array) return Float is
    Count: Integer;  -- Length of array
@@ -18,7 +18,7 @@ function Get_Minimum (Values: Values_Array) return Float is
    
 begin
    Count := Get_Count(Values);
-   Minimum := Values(0);
+   Minimum := Values(1);
    for index in Integer range 2 .. Count loop
       if Values(index) < Minimum then
          Minimum := Values(index);
@@ -26,7 +26,7 @@ begin
    end loop;
    return Minimum;
 end Get_Minimum;
-
+--------------------------------------------------------------
 
 function Get_Maximum (Values: Values_Array) return Float is
    Count: Integer;  -- Length of array
@@ -34,7 +34,7 @@ function Get_Maximum (Values: Values_Array) return Float is
    
 begin
    Count := Get_Count(Values);
-   Maximum := Values(0);
+   Maximum := Values(1);
    for index in Integer range 2 .. Count loop
       if Values(index) > Maximum then
          Maximum := Values(index);
@@ -42,7 +42,7 @@ begin
    end loop;
    return Maximum;
 end Get_Maximum;
-
+--------------------------------------------------------------
 
 function Get_Range (Values: Values_Array) return Float is
    CS_Range: Float;  -- Stores range
@@ -55,7 +55,7 @@ begin
    CS_Range := Maximum - Minimum;
    return CS_Range;
 end Get_Range;
-
+--------------------------------------------------------------
 
 function Get_Sum (Values: Values_Array) return Float is
    Sum: Float;
@@ -72,7 +72,7 @@ begin
    end loop;
    return Sum;
 end Get_Sum;
-
+--------------------------------------------------------------
 
 function Get_Mean (Values: Values_Array) return Float is
    Mean: Float;
@@ -88,7 +88,7 @@ begin
    Mean := Sum / Float(count);
    return Mean;
 end Get_Mean;
-
+--------------------------------------------------------------
 
 function Get_Sum_Of_Squares (Values: Values_Array) return Float is
     SS: Float;
@@ -109,7 +109,7 @@ begin
     end loop;
     return SS;
 end Get_Sum_Of_Squares;
-
+--------------------------------------------------------------
 
 function Get_Population_Variance (Values: Values_Array) return Float is
     Population_Variance : Float;
@@ -123,7 +123,7 @@ begin
     Population_Variance := Get_Sum_Of_Squares(Values) / Float(Count);
     return Population_Variance;
 end Get_Population_Variance;
-
+--------------------------------------------------------------
 
 function Get_Sample_Variance (Values: Values_Array) return Float is
     Sample_Variance : Float;
@@ -134,10 +134,10 @@ begin
     if count = 0 then
         return 0.0; -- should throw an error
     end if;
-    Sample_Variance := Get_Sum_Of_Squares(Values) / Float(Count - 2);
+    Sample_Variance := Get_Sum_Of_Squares(Values) / Float(Count - 1);
     return Sample_Variance;
 end Get_Sample_Variance;
-
+--------------------------------------------------------------
 
 function Get_Population_Standard_Deviation (Values: Values_Array) return Float is
     Population_Standard_Deviation : Float;
@@ -153,7 +153,7 @@ begin
     Population_Standard_Deviation := Ada.Numerics.Elementary_Functions.Sqrt(Population_Variance);
     return Population_Variance;
 end Get_Population_Standard_Deviation;
-
+--------------------------------------------------------------
 
 function Get_Sample_Standard_Deviation (Values: Values_Array) return Float is
     Sample_Standard_Deviation : Float;
@@ -169,7 +169,7 @@ begin
     Sample_Standard_Deviation := Ada.Numerics.Elementary_Functions.Sqrt(Sample_Variance);
     return Sample_Standard_Deviation;
 end Get_Sample_Standard_Deviation;
-
+--------------------------------------------------------------
 
 function Get_Standard_Error_of_Mean (Values : Values_Array) return Float is
     Standard_Error : Float;
@@ -183,6 +183,80 @@ begin
     Standard_Error := Get_Sample_Standard_Deviation(Values) / Ada.Numerics.Elementary_Functions.Sqrt(Float(Count));
     return Standard_Error;
 end Get_Standard_Error_of_Mean;
+--------------------------------------------------------------
+
+function Get_Coefficient_Of_Variation (Values : Values_Array) return Float is
+    Sample_Standard_Deviation : Float;
+    Mean : Float;
+    Count : Integer;
+
+begin
+    Count := Get_Count(Values);
+    if Count = 0 then
+        return 0.0; -- should throw an error
+    end if;
+    Sample_Standard_Deviation := Get_Sample_Standard_Deviation (Values);
+    Mean := Get_Mean (Values);
+    if Mean /= 0.0 then
+        return Sample_Standard_Deviation / Mean;
+    else 
+        return 0.0;
+    end if;
+end Get_Coefficient_Of_Variation;
+--------------------------------------------------------------
+
+function Is_Value_In_Array (Values : Values_Array; Value : Float) return Boolean is
+    Count : Integer;
+
+begin
+    Count := Get_Count(Values);
+    if Count = 0 then
+        return FALSE; -- should throw an error
+    end if;
+    for index in Integer range 1..Count loop
+        if Values(index) = Value then
+            return True;
+        end if;
+    end loop;
+    return false;
+end Is_Value_In_Array;
+--------------------------------------------------------------
+
+function Get_Unique_Values (Values : Values_Array) return Values_Array is
+    Count : INTEGER;
+    Index : INTEGER;
+    Value : FLOAT;
+    Matched : BOOLEAN;
+    Initial_Array : Values_Array (Integer range 1..Values'Length);
+
+begin
+    Count := Get_Count(Values);
+    Index := 2;
+    Initial_Array(1) := Values(1);
+    for idx in Integer range 2..Count loop
+        Value := Values(idx);
+        Matched := False;
+        for idy in Integer range 1..Index loop
+            if Value = Initial_Array(idy) then
+                Matched := True;
+                exit;
+            end if;
+        end loop;
+        if Matched = False then
+            Initial_Array(Index) := Value;
+            Index := Index + 1;
+        end if;
+    end loop;
+    Index := Index - 1;
+    -- Return array up to Index
+    return Initial_Array(1..Index);
+end Get_Unique_Values;
+--------------------------------------------------------------
+
+
+
+
+--------------------------------------------------------------
 
 end CompStats_Statistics;
 
